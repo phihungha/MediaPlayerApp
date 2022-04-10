4113aed62287a93458e74cbefa8b4fe04822604b
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-10 21:11:26 +0700

    Set adapter + layoutManager for VideoLibraryFragment's recyclerView and create some mock video objects to test

--- app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryFragment.java
+++ app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryFragment.java
@@ -9,8 +9,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mediaplayerapp.data.Video;
 import com.example.mediaplayerapp.databinding.FragmentVideoLibraryBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VideoLibraryFragment extends Fragment {
 
@@ -23,8 +29,18 @@ public class VideoLibraryFragment extends Fragment {
 
         binding = FragmentVideoLibraryBinding.inflate(inflater, container, false);
 
-        final TextView textView = binding.textVideoLibrary;
-        videoLibraryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        RecyclerView recyclerViewAllVideos = binding.allVideosRecyclerview;
+        List<Video> videos = new ArrayList<>();
+        videos.add(new Video(null, "video 1", 343));
+        videos.add(new Video(null, "video 2", 343));
+        videos.add(new Video(null, "video 3", 343));
+        videos.add(new Video(null, "video 4", 343));
+        videos.add(new Video(null, "video 5", 343));
+
+        recyclerViewAllVideos.setAdapter(new VideoLibraryItemAdapter(videos));
+        recyclerViewAllVideos.setLayoutManager(new GridLayoutManager(binding.getRoot().getContext(), 2));
+//        final TextView textView = binding.textVideoLibrary;
+//        videoLibraryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return binding.getRoot();
     }
 
