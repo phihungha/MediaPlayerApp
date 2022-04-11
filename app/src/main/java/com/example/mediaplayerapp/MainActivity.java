4eddf9ebd4243222460d58e9487b1698eeb67f19
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-11 20:21:12 +0700

    Revert "Add code related to permission to test the video library feature" and move all code related to permission to another branch to avoid conflicts later when PR
    This reverts commit 70c6513f

--- app/src/main/java/com/example/mediaplayerapp/MainActivity.java
+++ app/src/main/java/com/example/mediaplayerapp/MainActivity.java
@@ -1,18 +1,10 @@
 package com.example.mediaplayerapp;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,12 +12,9 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.mediaplayerapp.databinding.ActivityMainBinding;
 
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    ActivityResultLauncher<String[]> mPermissionResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,29 +32,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-        mPermissionResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions()
-                , new ActivityResultCallback<Map<String, Boolean>>() {
-                    @Override
-                    public void onActivityResult(Map<String, Boolean> result) {
-                    }
-                }
-        );
-        testAndRequestPermission();
     }
 
-    private void testAndRequestPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "thank you guys, u have already granted the permission. Enjoy the application!!", Toast.LENGTH_LONG).show();
-        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            Toast.makeText(this, "I need u to accept this, please. Check your permission in app setting!", Toast.LENGTH_LONG).show();
-            mPermissionResultLauncher.launch(new String[]{(Manifest.permission.READ_EXTERNAL_STORAGE)});
-
-        } else {
-            Toast.makeText(this, "hello u guys for the very FIRST TIME !!!!", Toast.LENGTH_LONG).show();
-            /* ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_CODE);*/
-            mPermissionResultLauncher.launch(new String[]{(Manifest.permission.READ_EXTERNAL_STORAGE)});
-        }
-    }
 }
