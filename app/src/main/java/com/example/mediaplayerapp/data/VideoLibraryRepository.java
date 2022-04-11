8998a44506484b8efbfc72f1a39a91bea7f84f60
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-11 20:19:10 +0700

    Add methods to "VideoLibraryRepository.java" to interact with "VideoDataSource.java" and higher layers

--- app/src/main/java/com/example/mediaplayerapp/data/VideoLibraryRepository.java
+++ app/src/main/java/com/example/mediaplayerapp/data/VideoLibraryRepository.java
@@ -1,4 +1,20 @@
 package com.example.mediaplayerapp.data;
 
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class VideoLibraryRepository {
+    private List<Video> mAllVideos;
+
+
+    public VideoLibraryRepository(Context context) {
+        mAllVideos = VideoDataSource.getAllVideos(context);
+    }
+
+    public List<Video> getAllVideos() {
+        return mAllVideos;
+    }
 }
