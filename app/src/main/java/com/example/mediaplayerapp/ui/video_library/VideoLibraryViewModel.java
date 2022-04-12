92d8ad42b76964463233663070ad01466cb99fbc
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-12 19:00:42 +0700

    Setup "VideoLibraryViewModel.java" to adopt MVVM architecture

--- app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryViewModel.java
+++ app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryViewModel.java
@@ -1,19 +1,28 @@
 package com.example.mediaplayerapp.ui.video_library;
 
+import android.content.Context;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.mediaplayerapp.data.Video;
+import com.example.mediaplayerapp.data.VideoLibraryRepository;
+
+import java.util.List;
+
 public class VideoLibraryViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private VideoLibraryRepository mVideoLibraryRepository;
+    private final MutableLiveData<List<Video>> mAllVideos;
 
-    public VideoLibraryViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is video library fragment");
+    public VideoLibraryViewModel(Context context) {
+        mVideoLibraryRepository = new VideoLibraryRepository(context);
+        mAllVideos = mVideoLibraryRepository.getAllVideos();
+        //mText.setValue("This is video library fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public MutableLiveData<List<Video>> getAllVideos() {
+        return mAllVideos;
     }
 }
