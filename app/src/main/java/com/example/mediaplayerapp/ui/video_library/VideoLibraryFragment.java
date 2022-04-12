07974005bfbf2830386e1d45d0a92b5b0e99f8f1
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-12 19:29:06 +0700

    Update "VideoLibraryFragment.java" to use ViewModel

--- app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryFragment.java
+++ app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryFragment.java
@@ -4,22 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.mediaplayerapp.data.Video;
-import com.example.mediaplayerapp.data.VideoLibraryRepository;
 import com.example.mediaplayerapp.databinding.FragmentVideoLibraryBinding;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class VideoLibraryFragment extends Fragment {
 
@@ -28,20 +21,10 @@ public class VideoLibraryFragment extends Fragment {
 
     private int mColumnCount = 1;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        videoLibraryViewModel = new ViewModelProvider(this).get(VideoLibraryViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentVideoLibraryBinding.inflate(inflater, container, false);
         View rootView = binding.getRoot();
-
-        VideoLibraryRepository videoLibraryRepository = new VideoLibraryRepository(getActivity());
-        List<Video> videos = videoLibraryRepository.getAllVideos();
-//        videos.add(new Video(null, "video 1", 343));
-//        videos.add(new Video(null, "video 2", 343));
-//        videos.add(new Video(null, "video 3", 343));
-//        videos.add(new Video(null, "video 4", 343));
-//        videos.add(new Video(null, "video 5", 343));
 
         RecyclerView recyclerViewAllVideos = binding.allVideosRecyclerview;
         if (mColumnCount <= 1) {
@@ -49,7 +32,22 @@ public class VideoLibraryFragment extends Fragment {
         } else {
             recyclerViewAllVideos.setLayoutManager(new GridLayoutManager(rootView.getContext(), 2));
         }
-        recyclerViewAllVideos.setAdapter(new VideoLibraryItemAdapter(videos));
+        VideoLibraryItemAdapter videoLibraryItemAdapter = new VideoLibraryItemAdapter();
+        recyclerViewAllVideos.setAdapter(videoLibraryItemAdapter);
+
+        videoLibraryViewModel =
+                new ViewModelProvider(getActivity()).get(VideoLibraryViewModel.class);
+        videoLibraryViewModel.getAllVideos().observe(getActivity(),videoList -> {
+            videoLibraryItemAdapter.updateVideoList(videoList);
+        });
+//        VideoLibraryRepository videoLibraryRepository = new VideoLibraryRepository(getActivity());
+//        List<Video> videos = videoLibraryRepository.getAllVideos();
+//        videos.add(new Video(null, "video 1", 343));
+//        videos.add(new Video(null, "video 2", 343));
+//        videos.add(new Video(null, "video 3", 343));
+//        videos.add(new Video(null, "video 4", 343));
+//        videos.add(new Video(null, "video 5", 343));
+
 
 //        final TextView textView = binding.textVideoLibrary;
 //        videoLibraryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
