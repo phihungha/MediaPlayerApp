04b66b3f40960e9cfdedd27f94416c6fd58ff017
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-12 19:43:37 +0700

    Fix bug - Creating a VideoLibraryViewModel instance with ViewModelProvider causes an exception
    Exception: "java.lang.RuntimeException: Cannot create an instance of class com.example.mediaplayerapp.ui.video_library.VideoLibraryViewModel"
    
    Solution: Change VideoLibraryViewModel to extends AndroidViewModel instead of ViewModel and create a new constructor
    
    Reference: https://stackoverflow.com/questions/48605672/cannot-create-an-instance-of-viewmodel-class-unable-to-start-activity-component

--- app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryViewModel.java
+++ app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryViewModel.java
@@ -1,7 +1,9 @@
 package com.example.mediaplayerapp.ui.video_library;
 
+import android.app.Application;
 import android.content.Context;
 
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -11,13 +13,14 @@ import com.example.mediaplayerapp.data.VideoLibraryRepository;
 
 import java.util.List;
 
-public class VideoLibraryViewModel extends ViewModel {
+public class VideoLibraryViewModel extends AndroidViewModel {
 
     private VideoLibraryRepository mVideoLibraryRepository;
     private final MutableLiveData<List<Video>> mAllVideos;
 
-    public VideoLibraryViewModel(Context context) {
-        mVideoLibraryRepository = new VideoLibraryRepository(context);
+    public VideoLibraryViewModel(Application application) {
+        super(application);
+        mVideoLibraryRepository = new VideoLibraryRepository(application);
         mAllVideos = mVideoLibraryRepository.getAllVideos();
         //mText.setValue("This is video library fragment");
     }
