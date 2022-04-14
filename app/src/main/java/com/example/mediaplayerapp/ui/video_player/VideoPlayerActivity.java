eb69ddd847379b4bf3e8ad5d4c67551a83ab7552
haphihung <haphihung55@gmail.com>
2022-04-14 23:53:25 +0700

    Integrate with media session

--- app/src/main/java/com/example/mediaplayerapp/ui/video_player/VideoPlayerActivity.java
+++ app/src/main/java/com/example/mediaplayerapp/ui/video_player/VideoPlayerActivity.java
@@ -3,6 +3,8 @@ package com.example.mediaplayerapp.ui.video_player;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -14,16 +16,20 @@ import androidx.core.view.WindowInsetsControllerCompat;
 import com.example.mediaplayerapp.databinding.ActivityVideoPlayerBinding;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector;
 
 import java.util.List;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
     public final static String VIDEO_URI_LIST = "VideoPlayerActivity.VIDEO_URI_LIST";
+
+    private final static String LOG_TAG = VideoPlayerActivity.class.getSimpleName();
     private final static String PLAYBACK_POSITION = "PLAYBACK_POSITION";
     private final static String CURRENT_MEDIA_ITEM_INDEX = "CURRENT_MEDIA_ITEM_INDEX";
 
     private ExoPlayer player;
+    private MediaSessionCompat mediaSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,15 @@ public class VideoPlayerActivity extends AppCompatActivity {
 
         player = new ExoPlayer.Builder(this).build();
         binding.videoPlayer.setPlayer(player);
+
+        mediaSession = new MediaSessionCompat(this, LOG_TAG);
+        mediaSession.setMediaButtonReceiver(null);
+        PlaybackStateCompat.Builder playbackStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(PlaybackStateCompat.ACTION_PLAY |
+                            PlaybackStateCompat.ACTION_PLAY_PAUSE);
+        mediaSession.setPlaybackState(playbackStateBuilder.build());
+        MediaSessionConnector mediaSessionConnector = new MediaSessionConnector(mediaSession);
+        mediaSessionConnector.setPlayer(player);
 
         if (savedInstanceState != null)
             startPlayback(savedInstanceState.getLong(PLAYBACK_POSITION),
@@ -90,14 +105,22 @@ public class VideoPlayerActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        mediaSession.setActive(true);
+    }
+
+    @Override
     protected void onPause() {
         super.onPause();
+        mediaSession.setActive(false);
         player.pause();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        mediaSession.release();
         player.release();
     }
 }
