8d357cd77d765bf928e64e2a1a58ec78d2c5145c
haphihung <haphihung55@gmail.com>
2022-04-15 23:10:02 +0700

    Navigate media queue in PiP mode

--- app/src/main/java/com/example/mediaplayerapp/ui/video_player/VideoPlayerActivity.java
+++ app/src/main/java/com/example/mediaplayerapp/ui/video_player/VideoPlayerActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.media.MediaDescriptionCompat;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,9 +18,12 @@ import androidx.core.view.WindowInsetsControllerCompat;
 import com.example.mediaplayerapp.databinding.ActivityVideoPlayerBinding;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector;
+import com.google.android.exoplayer2.ext.mediasession.TimelineQueueNavigator;
 
 import java.util.List;
+import java.util.Objects;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
@@ -48,21 +53,38 @@ public class VideoPlayerActivity extends AppCompatActivity {
                             PlaybackStateCompat.ACTION_PLAY_PAUSE);
         mediaSession.setPlaybackState(playbackStateBuilder.build());
         MediaSessionConnector mediaSessionConnector = new MediaSessionConnector(mediaSession);
+        mediaSessionConnector.setQueueNavigator(new TimelineQueueNavigator(mediaSession) {
+            @NonNull
+            @Override
+            public MediaDescriptionCompat getMediaDescription(@NonNull Player player, int windowIndex) {
+                return new MediaDescriptionCompat.Builder()
+                        .setTitle(Objects.requireNonNull(player.getCurrentMediaItem()).mediaMetadata.title)
+                        .setDescription("MediaDescription description for " + windowIndex)
+                        .setSubtitle("MediaDescription subtitle")
+                        .build();
+            }
+        });
         mediaSessionConnector.setPlayer(player);
 
-        playVideo();
+        loadMediaItemsFromIntent(getIntent());
+        play();
     }
 
     /**
-     * Begin video playback from intent's video URI(s).
+     * Load media items from intent into player.
      */
-    private void playVideo() {
-        Intent intent = getIntent();
+    private void loadMediaItemsFromIntent(Intent intent) {
         List<String> videoUris = intent.getStringArrayListExtra(VIDEO_URI_LIST);
         if (videoUris != null)
             videoUris.forEach(uri -> player.addMediaItem(MediaItem.fromUri(Uri.parse(uri))));
         else
             player.setMediaItem(MediaItem.fromUri(intent.getData()));
+    }
+
+    /**
+     * Begin playback of loaded media items.
+     */
+    private void play() {
         player.prepare();
         player.play();
     }
@@ -86,6 +108,15 @@ public class VideoPlayerActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        player.stop();
+        player.clearMediaItems();
+        loadMediaItemsFromIntent(intent);
+        play();
+    }
+
+    @Override
     protected void onUserLeaveHint() {
         super.onUserLeaveHint();
         if (player.isPlaying())
@@ -95,10 +126,7 @@ public class VideoPlayerActivity extends AppCompatActivity {
     @Override
     public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
         super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
-        if (isInPictureInPictureMode)
-            binding.videoPlayer.hideController();
-        else
-            binding.videoPlayer.showController();
+        binding.videoPlayer.setUseController(!isInPictureInPictureMode);
     }
 
     @Override
