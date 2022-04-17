d6e2caea29cd03abc43ee12e84cc0d7baebf3f06
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-17 14:18:03 +0700

    Remove code related to video playback in VideoLibraryFragment.java

--- app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryFragment.java
+++ app/src/main/java/com/example/mediaplayerapp/ui/video_library/VideoLibraryFragment.java
@@ -7,7 +7,6 @@ import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -19,14 +18,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.mediaplayerapp.databinding.FragmentVideoLibraryBinding;
 import com.example.mediaplayerapp.ui.video_player.VideoPlayerActivity;
 
-import java.io.File;
 import java.util.ArrayList;
 
 public class VideoLibraryFragment extends Fragment {
 
     private FragmentVideoLibraryBinding binding;
     VideoLibraryViewModel videoLibraryViewModel;
-
     private int mColumnCount = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -48,16 +45,7 @@ public class VideoLibraryFragment extends Fragment {
         videoLibraryViewModel.getAllVideos().observe(getActivity(), videoList -> {
             videoLibraryItemAdapter.updateVideoList(videoList);
         });
-        String path = Environment.getExternalStorageDirectory().getPath();
-        binding.videoPlayButton.setOnClickListener(view -> {
-            Intent startPlayback = new Intent(requireActivity(), VideoPlayerActivity.class);
-            ArrayList<String> videoUris = new ArrayList<>();
-//            videoUris.add(Uri.fromFile(new File(path + "/Download/video_sample.mp4")).toString());
-//            videoUris.add(Uri.fromFile(new File(path + "/Download/video_sample_2.mp4")).toString());
-//            videoUris.add(Uri.fromFile(new File(path + "/Download/video_sample_3.mp4")).toString());
-            startPlayback.putStringArrayListExtra(VideoPlayerActivity.VIDEO_URI_LIST, videoUris);
-            startActivity(startPlayback);
-        });
+
         return binding.getRoot();
     }
 
