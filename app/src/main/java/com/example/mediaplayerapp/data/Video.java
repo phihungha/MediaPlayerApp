0a74a8ab0bf0f4c67c917f0c3cae5874b07659e7
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-19 10:51:31 +0700

    Add comparators for Video class

--- app/src/main/java/com/example/mediaplayerapp/data/Video.java
+++ app/src/main/java/com/example/mediaplayerapp/data/Video.java
@@ -18,6 +18,35 @@ public class Video {
         this.duration = duration;
     }
 
+    public static Comparator<Video> VideoNameAZComparator = new Comparator<Video>() {
+        @Override
+        public int compare(Video video1, Video video2) {
+            return video1.getName().compareTo(video2.getName()) ;
+        }
+    };
+
+    public static Comparator<Video> VideoNameZAComparator = new Comparator<Video>() {
+        @Override
+        public int compare(Video video1, Video video2) {
+            return video2.getName().compareTo(video1.getName()) ;
+        }
+    };
+
+    public static Comparator<Video> VideoDurationAscendingComparator = new Comparator<Video>() {
+        @Override
+        public int compare(Video video1, Video video2) {
+            return video1.getDuration() - video2.getDuration() ;
+        }
+    };
+
+    public static Comparator<Video> VideoDurationDescendingComparator = new Comparator<Video>() {
+        @Override
+        public int compare(Video video1, Video video2) {
+            return video2.getDuration() - video1.getDuration() ;
+        }
+    };
+
+
     public Uri getUri() {
         return uri;
     }
