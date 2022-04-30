993611158355de2217fb8edb1acccf0c35ed7370
haphihung <haphihung55@gmail.com>
2022-04-30 23:12:51 +0700

    Add level list drawable for repeat button

--- app/src/main/java/com/example/mediaplayerapp/ui/music_player/MusicPlayerActivity.java
+++ app/src/main/java/com/example/mediaplayerapp/ui/music_player/MusicPlayerActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
+import com.example.mediaplayerapp.R;
 import com.example.mediaplayerapp.databinding.ActivityMusicPlayerBinding;
+
+import java.util.Objects;
 
 public class MusicPlayerActivity extends AppCompatActivity {
 
@@ -18,7 +23,10 @@ public class MusicPlayerActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.musicCloseBtn.setOnClickListener(view -> finish());
+        binding.musicSeekbar.setDuration(100);
+        binding.musicSeekbar.setPosition(50);
 
+        binding.repeatBtn.setImageLevel(1);
         binding.musicPlayPauseBtn.setOnClickListener(view -> binding.musicPlayPauseBtn.setImageLevel(1));
 
     }
