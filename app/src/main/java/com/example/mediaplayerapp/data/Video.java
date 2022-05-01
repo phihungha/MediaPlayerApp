f20bc5a2eae83f5ea605e048b452426554dddc56
DANH <nhutdanh175@gmail.com>
2022-05-01 12:32:13 +0700

    Merge Video from feature-video-library into dev_playlist

---
+++ app/src/main/java/com/example/mediaplayerapp/data/Video.java
@@ -0,0 +1,42 @@
+package com.example.mediaplayerapp.data;
+
+import android.net.Uri;
+
+import java.util.Comparator;
+
+public class Video {
+
+    public static Comparator<Video> VideoNameAZComparator =
+            Comparator.comparing(Video::getName);
+
+    public static Comparator<Video> VideoNameZAComparator =
+            (video1, video2) -> video2.getName().compareTo(video1.getName());
+
+    public static Comparator<Video> VideoDurationAscendingComparator =
+            Comparator.comparingInt(Video::getDuration);
+
+    public static Comparator<Video> VideoDurationDescendingComparator =
+            (video1, video2) -> video2.getDuration() - video1.getDuration();
+
+    private final Uri uri;
+    private final String name;
+    private final int duration;
+
+    public Video(Uri uri, String name, int duration) {
+        this.uri = uri;
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public Uri getUri() {
+        return uri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
