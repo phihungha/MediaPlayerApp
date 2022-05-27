4243d6350aa92c1f17a586467fc77de229b88b3a
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-05-27 23:07:08 +0700

    Add some necessary methods for MediaPlaybackInfoDao

--- app/src/main/java/com/example/mediaplayerapp/data/overview/MediaPlaybackInfoDao.java
+++ app/src/main/java/com/example/mediaplayerapp/data/overview/MediaPlaybackInfoDao.java
@@ -5,6 +5,7 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -14,16 +15,25 @@ public interface MediaPlaybackInfoDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(MediaPlaybackInfo mediaPlaybackInfo);
 
+    @Update
+    void update(MediaPlaybackInfo mediaPlaybackInfo);
+
+
     @Query("UPDATE media_playback_info_table " +
             "SET playbackAmount = playbackAmount + 1 " +
             "WHERE MediaPlaybackInfoID = :MediaPlaybackInfoID")
     void updatePlaybackAmount(int MediaPlaybackInfoID);
+
 
     @Query("SELECT * FROM media_playback_info_table " +
             "WHERE isVideo = 1 " +
             "ORDER BY lastPlaybackTime DESC " +
             "LIMIT 5")
     LiveData<List<MediaPlaybackInfo>> get5RecentVideos();
+
+    @Query("SELECT * FROM media_playback_info_table " +
+            "WHERE MediaURI = :mediaUri")
+    MediaPlaybackInfo getMediaPlayBackInfoByUri(String mediaUri);
 
     @Query("SELECT * FROM media_playback_info_table")
     LiveData<List<MediaPlaybackInfo>> getAllMediaPlaybackInfo();
